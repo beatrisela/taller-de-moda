b0778573fc0a4e3ef718db1019871bc8f1917aa0
beatriz <beatrisela@gmail.com>
2018-09-22 23:41:05 +0100

    refactorizar con nuevo modelo de acceso de datos

--- Nuevo Empleado.cs
+++ Nuevo Empleado.cs
@@ -14,8 +14,10 @@ namespace TALLER_DE_MODA
 {
     public partial class Nuevo_Empleado : Form
     {
-        public Nuevo_Empleado()
+        GestorDeDatosDeEmpleado gestor;
+        public Nuevo_Empleado(GestorDeDatosDeEmpleado gestor)
         {
+            this.gestor = gestor;
             InitializeComponent();
         }
 
@@ -34,15 +36,38 @@ namespace TALLER_DE_MODA
         }
         private void Boton_Registrar_Click(object sender, EventArgs e)
         {
-            if (p_nombres.Text != "")
+            if (p_nombres.Text == "" || Pertenese_DNI.Text == "")
             {
-                string sexoEmpleado = "";//para capturar la opcion del boton
+                MessageBoxButtons botones = MessageBoxButtons.OK;
+                MessageBox.Show("Error, El Empleado no se a podido insertar ", "Mensaje Empleado", botones, MessageBoxIcon.Error);
+            }
+            else
+            {
+                string sexoEmpleado = rdbHombre.Checked ? "M" : "H";
 
-                if (rdbHombre.Checked)
-                    sexoEmpleado = "H";
-                else if (rdbMujer.Checked)
-                    sexoEmpleado = "M";
+                //(string nombre, string DNI, string apellido1, string apellido2, string direccion, string fecha_inicio, string fecha_final, string sexo, string EMAIL, string observaciones)
+                EmpleadoModelo empleado = new EmpleadoModelo
+                {
+                    nombre = p_nombres.Text,
+                    dni = Pertenese_DNI.Text,
+                    apellido1 = p_apellido1.Text,
+                    apellido2 = p_apellido2.Text,
+                    direccion = p_direccion.Text,
+                    fecha_inicio = p_fecha_inicio.Text,
+                    fecha_final = p_fecha_final.Text,
+                    sexo = sexoEmpleado,
+                    email = textEMAIL.Text,
+                    observaciones = textobservaciones.Text
+                };
 
+                if (this.gestor.insert(empleado))
+                {
+                    empleado.idempleado = this.gestor.select(empleado.dni).idempleado;
+                    MessageBoxButtons botones = MessageBoxButtons.OK;
+                    MessageBox.Show("El Empleado ha  sido insertado correctamente", "Mensaje Empleado", botones, MessageBoxIcon.Information);
+                    Conexion.Insertar_TFNO_empleado(empleado.idempleado, p_telefono1.Text, p_telefono2.Text);
+                }
+                /*
                 if ((Conexion.Insertar_Empleados(p_nombres.Text, Pertenese_DNI.Text, p_apellido1.Text, p_apellido2.Text, p_direccion.Text, p_fecha_inicio.Text, p_fecha_final.Text, sexoEmpleado, textEMAIL.Text, textobservaciones.Text)) == true)
                 {
                     MessageBoxButtons botones = MessageBoxButtons.OK;
@@ -51,11 +76,7 @@ namespace TALLER_DE_MODA
                     // CONESTAMOS CON EL METODO INSERTAR_TFN_EMPEADO  Y LE PASAMOS LOS PARAMETROS Y LA CONEXION DE OBTENER_IDEMPLEADO
                     Conexion.Insertar_TFNO_empleado(Conexion.Obtener_idempleado(), p_telefono1.Text, p_telefono2.Text);
                 }
-            }
-            else
-            {
-                MessageBoxButtons botones = MessageBoxButtons.OK;
-                MessageBox.Show("Error, El Empleado no se a podido insertar ", "Mensaje Empleado", botones, MessageBoxIcon.Error);
+                */
             }
         }
 
